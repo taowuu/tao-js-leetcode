adc46533dcc4b6ae648d2b255508cdc68879e951
tao <2098613620@qq.com>
2022-08-16 11:55:40 +0800

    1

--- 122. 买卖股票的最佳时机 II.js
+++ 122. 买卖股票的最佳时机 II.js
@@ -1,16 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=122 lang=javascript
+ *
+ * [122] 买卖股票的最佳时机 II
+ */
+
+// @lc code=start
 /**
  * @param {number[]} prices
  * @return {number}
  * https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-ii/
  */
-var maxProfit = function(prices) {
-    if(prices.length === 0) return 0
-    p = prices
+ var maxProfit = function(prices) {
+    // 贪心
+    if(prices.length === 1) return 0
     let res = 0
-    for(let i = 1; i < p.length; i++) {
-        if(p[i - 1] < p[i]) {
-            res += p[i] - p[i - 1]
+    for(let i = 1; i < prices.length; i++) {
+        if(prices[i] > prices[i - 1]) {
+            res += prices[i] - prices[i - 1]
         }
-    }    
+    }
     return res
 };
+// @lc code=end
+
