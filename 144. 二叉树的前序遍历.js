9d430be2ceaf87529d1693e754ec6a6195737eb8
tao <2098613620@qq.com>
2022-08-19 10:14:39 +0800

    1

--- 144. 二叉树的前序遍历.js
+++ 144. 二叉树的前序遍历.js
@@ -1,3 +1,10 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=javascript
+ *
+ * [144] 二叉树的前序遍历
+ */
+
+// @lc code=start
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -12,25 +19,18 @@
  * https://leetcode.cn/problems/binary-tree-preorder-traversal/
  */
  var preorderTraversal = function(root) {
-  // dfs 递归法
-  // 边界处理
-  if(root == null) {
-      return []
-  }
-  // 全局答案
-  var res = []
-  dfs(root, res)
+  // 递归
+  let res = []
+  helper(root, res)
   return res
 };
-var dfs = function(node, res) {
-  // 终止条件
-  if(node == null) {
-      return
+const helper = (node, res) => {
+  if(node === null) {
+      return res
   }
-  // 无需向子问题要答案
-  // 当前递归层操作
   res.push(node.val)
-  dfs(node.left, res)
-  dfs(node.right, res)
-  // 无需返回结果
+  helper(node.left, res)
+  helper(node.right, res)
 }
+// @lc code=end
+
