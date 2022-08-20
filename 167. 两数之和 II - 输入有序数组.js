57a3e336e734aa06d252e1307250bea798b948a1
tao <2098613620@qq.com>
2022-08-20 11:53:59 +0800

    1

--- 167. 两数之和 II - 输入有序数组.js
+++ 167. 两数之和 II - 输入有序数组.js
@@ -1,12 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=167 lang=javascript
+ *
+ * [167] 两数之和 II - 输入有序数组
+ */
+
+// @lc code=start
 /**
  * @param {number[]} numbers
  * @param {number} target
  * @return {number[]}
  * https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/
  */
-var twoSum = function(numbers, target) {
-    let left = 0
-    let right = numbers.length - 1
+ var twoSum = function(numbers, target) {
+    // 反向双指针
+    let left = 0,
+        right = numbers.length - 1
     while(left <= right) {
         let sum = numbers[left] + numbers[right]
         if(sum === target) return [left + 1, right + 1]
@@ -14,3 +22,5 @@ var twoSum = function(numbers, target) {
         else right--
     }
 };
+// @lc code=end
+
