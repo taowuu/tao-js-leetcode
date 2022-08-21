317cfe5efe31093a6c2c8c23c52f611cbc890414
tao <2098613620@qq.com>
2022-08-21 12:13:36 +0800

    1

--- 198. 打家劫舍.js
+++ 198. 打家劫舍.js
@@ -1,26 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=198 lang=javascript
+ *
+ * [198] 打家劫舍
+ */
+
+// @lc code=start
 /**
  * @param {number[]} nums
  * @return {number}
  * https://leetcode.cn/problems/house-robber/
  */
-var rob = function(nums) {
-    // 
-    if(nums === null || nums.length === 0) {
-        return 0
-    }
+ var rob = function(nums) {
     if(nums.length === 1) return nums[0]
-    // 定义 dp
-    // 0 - i 能偷到的最大值
     let dp = new Array(nums.length)
-    // 初始
     dp[0] = nums[0]
     dp[1] = Math.max(nums[0], nums[1])
     let res = Math.max(dp[0], dp[1])
-    // 
     for(let i = 2; i < nums.length; i++) {
         dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i])
         res = Math.max(dp[i], res)
     }
-    // 
     return res
 };
+// @lc code=end
+
