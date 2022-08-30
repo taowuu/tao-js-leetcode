f670dd79620cb1ae673bfe891c5702d7e0f77185
tao <2098613620@qq.com>
2022-08-30 19:58:53 +0800

    1

--- 300. 最长递增子序列.js
+++ 300. 最长递增子序列.js
@@ -8,7 +8,7 @@ var lengthOfLIS = function(nums) {
     let dp = new Array(n).fill(1)
     for(let i = 0; i < n; i++) {
         for(let j = 0; j < i; j++) {
-            if(nums[i] > nums[j]) {
+            if(nums[j] < nums[i]) {
                 dp[i] = Math.max(dp[i], dp[j] + 1)
             }
         }
