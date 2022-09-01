139823cd28e6b8beff470433cc783647fa733428
tao <2098613620@qq.com>
2022-09-02 07:35:25 +0800

    1

--- 455. 分发饼干.js
+++ 455. 分发饼干.js
@@ -1,3 +1,10 @@
+/*
+ * @lc app=leetcode.cn id=455 lang=javascript
+ *
+ * [455] 分发饼干
+ */
+
+// @lc code=start
 /**
  * @param {number[]} g
  * @param {number[]} s
@@ -5,27 +12,6 @@
  * https://leetcode.cn/problems/assign-cookies/
  */
  var findContentChildren = function(g, s) {
-    // 排序
-    g.sort((a, b) => a - b);
-    s.sort((a, b) => a - b)
-    // 用最小饼干满足胃口最小孩子
-    let res = 0
-    let i = 0
-    let j = 0
-    while(i < g.length && j < s.length) {
-        // 饼干满足
-        if(g[i] <= s[j]) {
-            res += 1
-            i++
-            j++
-        } else {
-            // 饼干不满足
-            j++
-        }
-    }
-    return res
-};
-var findContentChildren = function(g, s) {
     g.sort((a, b) => a - b)
     s.sort((a, b) => a - b)
     // 用最大饼干满足胃口最大孩子
@@ -39,3 +25,5 @@ var findContentChildren = function(g, s) {
     }
     return res
 };
+// @lc code=end
+
