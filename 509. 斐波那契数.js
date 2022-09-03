34c1fc71c5594c4a528694558c8978de1ecb6aad
tao <2098613620@qq.com>
2022-09-03 10:13:33 +0800

    1

--- 509. 斐波那契数.js
+++ 509. 斐波那契数.js
@@ -1,25 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=javascript
+ *
+ * [509] 斐波那契数
+ */
+
+// @lc code=start
 /**
  * @param {number} n
  * @return {number}
  * https://leetcode.cn/problems/fibonacci-number/
  */
-var fib = function(n) {
-    // 自底向上 test
-    let a = [0, 1]
-    for(let i = 2; i < n; i++) {
-        a[i] = a[i - 1] + a[i - 2]
-    }
-    return a[i]
-};
-var fib = function(n) {
+ var fib = function(n) {
     if(n <= 1) return n
-    let dp1 = 0
-    let dp2 = 1
-    let dp3 = 0
-    for(let i = 2; i <= n; i++) {
-        dp3 = dp1 + dp2
-        dp1 = dp2
-        dp2 = dp3
+    let f0 = 0,
+        f1 = 1,
+        i = 2
+    while(i <= n) {
+        f2 = f0 + f1
+        f0 = f1
+        f1 = f2
+        i++
     }
-    return dp3
+    return f2
 };
+// @lc code=end
+
