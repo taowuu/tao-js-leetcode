b31fb162b3eab0c719d69c87285570496149fb75
tao <2098613620@qq.com>
2022-09-06 08:20:06 +0800

    1

--- 977. 有序数组的平方.js
+++ 977. 有序数组的平方.js
@@ -1,9 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=977 lang=javascript
+ *
+ * [977] 有序数组的平方
+ */
+
+// @lc code=start
 /**
  * @param {number[]} nums
  * @return {number[]}
  * https://leetcode.cn/problems/squares-of-a-sorted-array/
  */
-var sortedSquares = function(nums) {
+ var sortedSquares = function(nums) {
     let n = nums.length
     let arr = new Array(n),
         left = 0,
@@ -23,3 +30,5 @@ var sortedSquares = function(nums) {
     }
     return arr
 };
+// @lc code=end
+
