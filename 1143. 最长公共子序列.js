ea025045299190246c39d3559250d2747846afb4
tao <2098613620@qq.com>
2022-09-07 08:07:32 +0800

    1

--- 1143. 最长公共子序列.js
+++ 1143. 最长公共子序列.js
@@ -4,15 +4,13 @@
  * @return {number}
  * https://leetcode.cn/problems/longest-common-subsequence/
  */
- var longestCommonSubsequence = function(text1, text2) {
+var longestCommonSubsequence = function(text1, text2) {
     let a1 = text1.split("")
     let a2 = text2.split("")
     let n1 = a1.length
     let n2 = a2.length
-    // 初始状态
     let dp = new Array(n1 + 1).fill(0)
                 .map(() => new Array(n2 + 1).fill(0))
-    // 状态转移
     for(let i = 1; i < n1 + 1; i++) {
         for(let j = 1; j < n2 + 1; j++) {
             if(a1[i - 1] === a2[j - 1]) {
@@ -22,6 +20,5 @@
             }
         }
     }
-    // 返回最终状态
     return dp[n1][n2]
 };
