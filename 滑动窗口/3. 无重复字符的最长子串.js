233a2db947292e7e7d63c1b40aa29441d5cde821
tao <47629165+taowuu@users.noreply.github.com>
2022-10-10 20:12:25 +0800

    1

--- 滑动窗口/3. 无重复字符的最长子串.js
+++ 滑动窗口/3. 无重复字符的最长子串.js
@@ -1,16 +1,21 @@
 // https://leetcode.cn/problems/longest-substring-without-repeating-characters/
 
 var lengthOfLongestSubstring = function(s) {
-    let l = 0
+    let l = 0,
+        r = 0
     const memo = {}
     let res = 0
 
-    for(let r = 0; r < s.length; r++) {
-        if(s[r] in memo && memo[s[r]] >= l) {
+    while(r < s.length) {
+        let c = s[r]
+
+        if(c in memo && memo[c] >= l) {
             l = memo[s[r]] + 1
         }
+
         res = Math.max(res, r - l + 1)
         memo[s[r]] = r
+        r++
     }
 
     return res
---
+++ 滑动窗口/76. 最小覆盖子串.js
@@ -0,0 +1,5 @@
+// https://leetcode.cn/problems/minimum-window-substring/
+
+var minWindow = function(s, t) {
+    
+}
