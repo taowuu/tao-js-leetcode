c3060cb28df14ebc017d57b074510dacd5ff2d0d
tao <47629165+taowuu@users.noreply.github.com>
2022-10-11 19:10:00 +0800

    Create 104. 二叉树的最大深度.js

---
+++ 深度优先/104. 二叉树的最大深度.js
@@ -0,0 +1,18 @@
+var maxDepth = function(root) {
+    let res = 0
+
+    const dfs = (n, l) => {
+        if(!n) return
+
+        if(!n.left && !n.right) {
+            res = Math.max(res, l)
+        }
+        
+        dfs(n.left, l + 1)
+        dfs(n.right, l + 1)
+    }
+
+    dfs(root, 1)
+
+    return res
+}
