08c2cf139bc3c559c9d48b071d56ae0e628bfdc5
tao <47629165+taowuu@users.noreply.github.com>
2022-10-12 12:10:10 +0800

    1

--- 深度优先/112. 路径总和.js
+++ 深度优先/112. 路径总和.js
@@ -1,13 +1,13 @@
 // https://leetcode.cn/problems/path-sum/
 
-var hasPathSum = function(root, targetSum) {
+var hasPathSum = function(root, target) {
     if(!root) return false
 
     let res = false
 
     const dfs = (n, s) => {
         if(!n.left && !n.right) {
-            if(s === targetSum) {
+            if(s === target) {
                 res = true
             }
         }
