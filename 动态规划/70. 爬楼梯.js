b2f0f79c13a685584ea44fef036757f55a96c6eb
tao <47629165+taowuu@users.noreply.github.com>
2022-10-12 15:42:41 +0800

    Create 70. 爬楼梯.js

---
+++ 动态规划/70. 爬楼梯.js
@@ -0,0 +1,13 @@
+// https://leetcode.cn/problems/climbing-stairs/
+
+var climbStairs = function(n) {
+    if(n < 2) return 1
+
+    const dp = [1, 1]
+    
+    for(let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+
+    return dp[n]
+}
