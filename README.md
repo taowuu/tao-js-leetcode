800f0ccb234a271b208da1b266ed2af281b1fc93
Taowuu <taowuu@foxmail.com>
2022-11-06 21:11:30 +0800

    add

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # tao-leetcode
 
+
 ## 动态规划
+- [70. 爬楼梯](https://github.com/taowuu/tao-leetcode/blob/main/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92/70.%20%E7%88%AC%E6%A5%BC%E6%A2%AF.js)
+
+- [198. 打家劫舍](https://github.com/taowuu/tao-leetcode/blob/main/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92/198.%20%E6%89%93%E5%AE%B6%E5%8A%AB%E8%88%8D.js)
 
--- 动态规划/198. 打家劫舍.js
+++ 动态规划/198. 打家劫舍.js
@@ -1,13 +1,22 @@
 // https://leetcode.cn/problems/house-robber/
 
 var rob = function(nums) {
-    if(nums.length === 0) return 0
-
+    // 边界处理
+    if(nums.length === 0 || !nums) return 0
+    // 定义：
+    // 0 号房金额为 0
+    // 1 号房金额为 nums[0]
+    // i 号房金额为 nums[i]
     const dp = [0, nums[0]]
-
+    // i 号房金额由 
+    // i-2 号房和本房
+    // 或是
+    // i-1 号房和不偷本房
+    // 中的最大金额决定
+    // dp[i - 2] + nums[i - 1]
     for(let i = 2; i <= nums.length; i++) {
         dp[i] = Math.max(dp[i - 2] + nums[i - 1], dp[i - 1])
     }
-
+    // 返回最终金额
     return dp[nums.length]
 }
