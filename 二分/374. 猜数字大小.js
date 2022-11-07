ecd7ade21136f00b8790841a72e1f0761c94db1f
Taowuu <taowuu@foxmail.com>
2022-11-07 08:58:44 +0800

    add

--- 二分/374. 猜数字大小.js
+++ 二分/374. 猜数字大小.js
@@ -1,19 +1,19 @@
 // https://leetcode.cn/problems/guess-number-higher-or-lower/
 
 var guessNumber = function(n) {
-    let l = 0,
-        h = n
-    
-    while(l <= h) {
-        let m = Math.floor((l + h) / 2)
-        let res = guess(m)
+    let start = 0,
+        end = n
+    // 相等时也要猜测这个数
+    while(start <= end) {
+        let mid = Math.floor((start + end) / 2)
+        let res = guess(mid)
 
         if(res === 0) {
-            return m
+            return mid
         } else if(res === 1) {
-            l = m + 1
+            start = mid + 1
         } else {
-            h = m - 1
+            end = mid - 1
         }
     }
 }
