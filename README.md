ea2a5315d0465e8d609b144811361f5a57374189
Taowuu <taowuu@foxmail.com>
2022-11-07 08:39:32 +0800

    add

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # tao-leetcode
 
-
 ## 动态规划
 - [70. 爬楼梯](https://github.com/taowuu/tao-leetcode/blob/main/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92/70.%20%E7%88%AC%E6%A5%BC%E6%A2%AF.js)
 
 - [198. 打家劫舍](https://github.com/taowuu/tao-leetcode/blob/main/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92/198.%20%E6%89%93%E5%AE%B6%E5%8A%AB%E8%88%8D.js)
 
+## 队列
+- [933. 最近的请求次数](https://github.com/taowuu/tao-leetcode/blob/main/%E9%98%9F%E5%88%97/933.%20%E6%9C%80%E8%BF%91%E7%9A%84%E8%AF%B7%E6%B1%82%E6%AC%A1%E6%95%B0.js)
+
+## 
--- 队列/933. 最近的请求次数.js
+++ 队列/933. 最近的请求次数.js
@@ -1,15 +1,19 @@
 // https://leetcode.cn/problems/number-of-recent-calls/
 
 var RecentCounter = function() {
-    this.q = []
+    // 初始化计数器，请求数为 0
+    // 先进后出使用队列
+    this.queue = []
 }
 
 RecentCounter.prototype.ping = function(t) {
-    this.q.push(t)
-
+    // 入队一个请求
+    this.queue.push(t)
+    // 队头在 3000ms 外出队
     while(this.q[0] < t - 3000) {
-        this.q.shift()
+        this.queue.shift()
     }
-
-    return this.q.length
+    // 最终队列长度为
+    // 在 [t-3000, t] 内发生的请求数
+    return this.queue.length
 }
