57452109fd8ec883f5c7f0a2552af90bd48255e5
Taowuu <taowuu@foxmail.com>
2022-11-07 09:12:47 +0800

    add

--- 反向双指针/977. 有序数组的平方.js
+++ 反向双指针/977. 有序数组的平方.js
@@ -1,21 +1,28 @@
 // https://leetcode.cn/problems/squares-of-a-sorted-array/
 
 var sortedSquares = function(nums) {
+    // 边界处理
+    if(nums.length === 0) return []
+    // 反向双指针
     const res = []
-    let r = nums.length - 1,
-        l = 0
-    let end = r
+    let right = nums.length - 1,
+        left = 0
+    // 极大值只会在数字首尾产生
+    // 从后往前填充数组
+    let end = right
+    // 同一个值也要填入数组
+    while(left <= right) {
+        let rv = Math.pow(nums[right], 2)
+        let lv = Math.pow(nums[left], 2)
 
-    while(l <= r) {
-        let rv = nums[r] * nums[r]
-        let lv = nums[l] * nums[l]
         if(rv > lv) {
             res[end] = rv
-            r--
+            right--
         } else {
             res[end] = lv
-            l++
+            left++
         }
+
         end--
     }
 
