4ad883e2ea1d1bb0696b2cea802915e53aeb4188
Taowuu <taowuu@foxmail.com>
2022-11-08 10:11:10 +0800

    Update 141. 环形链表.js

--- 同向双指针/141. 环形链表.js
+++ 同向双指针/141. 环形链表.js
@@ -1,14 +1,14 @@
 // https://leetcode.cn/problems/linked-list-cycle/
 
 var hasCycle = function(head) {
-    // 快慢指针相遇说明有环
+    // 快慢指针
     let fast = head,
         slow = head
     // 防止 fast.next.next 报错
     while(fast && fast.next) {
         fast = fast.next.next
         slow = slow.next
-        
+        // 指针相遇说明有环
         if(fast === slow) return true
     }
 
