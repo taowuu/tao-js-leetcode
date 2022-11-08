792a42122003ca6151898f317815a920289a149d
Taowuu <taowuu@foxmail.com>
2022-11-08 10:32:22 +0800

    Update 206. 反转链表.js

--- 同向双指针/206. 反转链表.js
+++ 同向双指针/206. 反转链表.js
@@ -1,15 +1,18 @@
 // https://leetcode.cn/problems/reverse-linked-list/
 
 var reverseList = function(head) {
-    let l = null,
-        r = head
-    
-    while(r) {
-        let temp = r.next
-        r.next = l
-        l = r
-        r = temp
+    let left = null,
+        right = head
+    // null 1 -> 2 -> 3
+    // l    r    t
+    // null <- 1  2 -> 3
+    //         l  r    t 
+    while(right) {
+        let temp = right.next // 2
+        right.next = left // 1->null
+        left = right // 1
+        right = temp // 2
     }
-
-    return l
+    // 最后 right 为 null
+    return left
 }
