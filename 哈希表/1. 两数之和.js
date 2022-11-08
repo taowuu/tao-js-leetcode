42b8692240768a0102fd5c0d86552b87074a9362
Taowuu <taowuu@foxmail.com>
2022-11-08 10:38:36 +0800

    Update 1. 两数之和.js

--- 哈希表/1. 两数之和.js
+++ 哈希表/1. 两数之和.js
@@ -1,13 +1,18 @@
 // https://leetcode.cn/problems/two-sum/
 
 var twoSum = function(nums, target) {
+    // 哈希表缓存
     const memo = {}
-
+    
     for(let i = 0; i < nums.length; i++) {
+        // 7 = 9 - 2
         let diff = target - nums[i]
+        // 找到 7
         if(diff in memo) {
             return [i, memo[diff]]
         } else {
+            // 找不到存入 2
+            // 下次走到 7 就可以返回这个 2
             memo[nums[i]] = i
         }
     }
