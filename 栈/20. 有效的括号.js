8a2ffb965eab37bfce562db66ab7ee7c6ac8f001
Taowuu <taowuu@foxmail.com>
2022-11-08 10:50:14 +0800

    Update 20. 有效的括号.js

--- 栈/20. 有效的括号.js
+++ 栈/20. 有效的括号.js
@@ -1,21 +1,26 @@
 // https://leetcode.cn/problems/valid-parentheses/
 
 var isValid = function(str) {
-    const s = []
+    // 括号匹配使用栈
+    const stack = []
+    // 缓存匹配关系
     const memo = {
         '(': ')',
         '[': ']',
         '{': '}'
     }
-
-    for(i of str) {
-        if(i in memo) {
-            s.push(i)
+    
+    for(value of str) {
+        if(value in memo) {
+            // 遇到左括号入栈
+            stack.push(value)
         } else {
-            let c = s && s.pop()
-            if(i !== memo[c]) return false
+            // 遇到右括号出栈
+            let char = stack && stack.pop()
+            // 判断左右括号是否匹配
+            if(value !== memo[char]) return false
         }
     }
-
-    return s.length === 0
+    // 确保括号完全被匹配
+    return stack.length === 0
 }
